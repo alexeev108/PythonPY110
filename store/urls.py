28a8dc9bb2e89a6318c79fb5b53cc2dd897c42e7
alexeev108 <alexeev108@yandex.ru>
2024-05-31 00:40:55 +0300

    feat: Добавлена возможность добавления скидочного купона и расчета стоимости доставки (сделаны пункты 1.1-1.2 Лабораторной работы #5)

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from store.views import products_view, shop_view, products_page_view, cart_view, cart_add_view, cart_del_all_view, \
-    cart_del_one_view
+    cart_del_one_view, coupon_check_view, delivery_estimate_view
 
 app_name = 'store'
 
@@ -14,4 +14,6 @@ urlpatterns = [
     path('cart/add/<str:id_product>', cart_add_view),
     path('cart/del/<str:id_product>', cart_del_all_view),
     path('cart/del_one/<str:id_product>', cart_del_one_view),
+    path('coupon/check/<slug:name_coupon>', coupon_check_view),
+    path('delivery/estimate/', delivery_estimate_view),
 ]
--- store/views.py
+++ store/views.py
@@ -186,3 +186,51 @@ def cart_del_one_view(request, id_product):
             return JsonResponse({"answer": "Неудачное удаление из корзины"},
                             status=404,
                             json_dumps_params={'ensure_ascii': False})
+
+def coupon_check_view(request, name_coupon):
+    DATA_COUPON = {
+        "coupon": {
+            "value": 10,
+            "is_valid": True},
+        "coupon_old": {
+            "value": 20,
+            "is_valid": False},
+        "coupon_new": {
+            "value": 30,
+            "is_valid": True},
+    }
+    if request.method == "GET":
+        if name_coupon and name_coupon in DATA_COUPON:
+            return JsonResponse({"discount": DATA_COUPON[name_coupon]["value"],
+                                 "is_valid": DATA_COUPON[name_coupon]["is_valid"]},
+                                json_dumps_params={'ensure_ascii': False})
+        else:
+            return HttpResponseNotFound("Неверный купон")
+
+def delivery_estimate_view(request):
+    DATA_PRICE = {
+        "Россия": {
+            "Москва": {"price": 80},
+            "Санкт-Петербург": {"price": 140},
+            "fix_price": 100,
+        },
+    }
+    if request.method == "GET":
+        data = request.GET
+        country = data.get('country')
+        city = data.get('city')
+        if country or city:
+            try:
+                error = DATA_PRICE[country]
+                if (country in DATA_PRICE) and (city in DATA_PRICE[country]):
+                    for item_ in DATA_PRICE[country].keys():
+                        if item_ == city:
+                            return JsonResponse({"price": DATA_PRICE[country][city]["price"]},
+                                                json_dumps_params={'ensure_ascii': False})
+                else:
+                    return JsonResponse({"price": DATA_PRICE[country]["fix_price"]},
+                                        json_dumps_params={'ensure_ascii': False})
+            except:
+                return HttpResponseNotFound("Неверные данные")
+        else:
+            return HttpResponseNotFound("Неверные данные")
